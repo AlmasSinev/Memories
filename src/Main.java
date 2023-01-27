85d04d5ac5865fb2531c85e8794a28a090ccb379
AlmasSinev <26alamssinev@gmail.com>
2023-01-27 16:59:58 +0300

    random numbers

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
         System.out.println("Hello Me!");
+
+        Random random = new Random();
+
+        for (int i=0; i<10; i++) {
+            int x = random.nextInt(100) + 1;
+            System.out.print(x + " ");
+        }
+
     }
 }
