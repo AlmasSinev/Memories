17a9d5184af547cdb9fb4da30e9bf93769b04c10
AlmasSinev <26alamssinev@gmail.com>
2023-01-27 17:25:19 +0300

    printf

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,9 @@
-import javax.swing.*;
-import java.util.ArrayList;
+
 
 public class Main {
     public static void main(String[] args) {
 
-        ArrayList<String> food = new ArrayList<>();
-
-        food.add("pizza");
-        food.add("ramen");
-        food.add("sushi");
-
-        for (String f : food) {
-            System.out.println(f);
-        }
-
+        System.out.printf("This is a format string %d", 123);
 
     }
 }
